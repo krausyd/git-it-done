d0ecf10cdb3a6b6cb8ca71e1db64b1cc3e024291
fernkari (∩◕-◕ )⊃━✿✿✿✿ <fernkari@amazon.com>
2021-11-25 15:45:51 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Add a form to request username to get repos from

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,4 +8,19 @@ const getUserRepos = function(user) {
         });
 };
 
-getUserRepos("krausyd");
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
